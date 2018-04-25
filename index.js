ca4ac13bf63e651e2e6362198d337cfaf31e3009
Matteo Minardi <minardi.matteo@hotmail.it>
2018-04-25 19:02:15 +0200

    Setup routing and views EJS

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 /* SETUP */
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const production = "production" === process.env.NODE_ENV;
+const production = 'production' === process.env.NODE_ENV;
 const port = process.env.PORT || 3000;
 
-const morgan = require("morgan");
+const morgan = require('morgan');
 if (!production) {
-  app.use(morgan("dev"));
+  app.use(morgan('dev'));
 }
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/* ROUTING */
-const productRoutes = require("./src/routes/products");
-app.use("/", mainRoutes);
+const path = require('path');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/src/views'));
 
+/* ROUTING */
+const mainRoutes = require('./src/routes/main');
+app.use('/', mainRoutes);
+
+/* SERVER LISTEN */
 app.listen(port, startListening);
 
 function startListening() {
   if (!production) {
-    console.log("Server available on http://localhost:" + port);
+    console.log('Server available on http://localhost:' + port);
   }
 }
